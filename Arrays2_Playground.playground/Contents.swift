ab0e591ac6e14a76c0e002b77aa98ee56623751e
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-04-05 21:54:55 +0100

    Array2_Playground

---
+++ Arrays2_Playground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var vazio: [Int] = []
+
+var idades = [4,12,54,34,22,5,13,22]
+
+idades[3]
+
+idades.append(10)
+
+idades.insert(44, at: 0)
+
+print(idades)
+
+idades.sort()
+print(idades)
