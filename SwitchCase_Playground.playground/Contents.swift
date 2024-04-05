4587ddc07f712629c2acccaf18bd448818d219d0
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-04-05 22:28:03 +0100

    SwitchCase_Playground

---
+++ SwitchCase_Playground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let codigo = $
+
+switch codigo{
+    case 1: print("Um")
+    case 2: print("Dois")
+    case 3: print("Três")
+    case 4: print("Quatro")
+    default: print("Outro valor")
+}
