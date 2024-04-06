27850415d3292c17d419a0b5c9f21f5e3a25736d
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-04-06 20:13:30 +0100

    Classes_Construtor_Playground

---
+++ Playground/Classes_Playground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class Pessoa {
+    var nome: String
+    var email: String
+    var idade: Int
+    
+    init(nome: String, email: String, idade: Int) {
+        self.nome = nome
+        self.email = email
+        self.idade = idade
+    }
+}
+
+let p1 = Pessoa(nome: "Maria", email: "maria@email.com", idade: 30)
